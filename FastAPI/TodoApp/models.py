2123c484cdb0b5f78290577bcca01a7117550169
Joshua Garvey <joshua.garvey@pointduty.com.au>
2022-03-02 16:41:54 +1000

    updating Todos schema

--- FastAPI/TodoApp/models.py
+++ FastAPI/TodoApp/models.py
@@ -1,5 +1,22 @@
-from sqlalchemy import Boolean, Column, Integer, String
+from sqlalchemy import Boolean, Column, Integer, String, ForeignKey
+from sqlalchemy.orm import relationship
 from database import Base
+
+
+# class describing users table
+class Users(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, index=True)
+    email = Column(String, unique=True, index=True)
+    username = Column(String, unique=True, index=True)
+    first_name = Column(String)
+    last_name = Column(String)
+    # hashed password is an encrypted version of a password which cannot be reversed to find the original
+    hashed_password = Column(String)
+    is_active = Column(Boolean, default=True)
+
+    todos = relationship("Todos", back_populates="owner")
 
 
 # class describing Todos table (inherits from Base)
@@ -11,4 +28,6 @@ class Todos(Base):
     description = Column(String)
     priority = Column(Integer)
     complete = Column(Boolean, default=False)
-    
+    owner_id = Column(Integer, ForeignKey("users.id"))
+
+    owner = relationship("Users", back_populates="todos")
