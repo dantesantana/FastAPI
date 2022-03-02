3b16a9b7e689833fb24a3764ac4e28da06114929
Joshua Garvey <joshua.garvey@pointduty.com.au>
2022-03-02 12:03:09 +1000

    setting up first SQL API functions

--- FastAPI/TodoApp/main.py
+++ FastAPI/TodoApp/main.py
@@ -1,12 +1,22 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
 import models
-from database import engine
+from database import engine, SessionLocal
+from sqlalchemy.orm import Session
 
 app = FastAPI()
 
 models.Base.metadata.create_all(bind=engine)
 
 
+def get_db():
+    try:
+        db = SessionLocal()
+        yield db
+    finally:
+        db.close()
+
+
+# read all entries in a table with dependencies i.e. AFTER the get_db() function is executed
 @app.get("/")
-async def create_database():
-    return {"Database": "Created"}
+async def read_all(db: Session = Depends(get_db)):
+    return db.query(models.Todos).all()
