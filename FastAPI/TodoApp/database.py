b4ee48fc3611feff81c902a79404d1115b7b5a04
Joshua Garvey <joshua.garvey@pointduty.com.au>
2022-03-04 14:21:22 +1000

    MySQL implementation

--- FastAPI/TodoApp/database.py
+++ FastAPI/TodoApp/database.py
@@ -5,11 +5,13 @@ from sqlalchemy.ext.declarative import declarative_base
 # connect to a sqlite database & create file "todos.db" storing our database
 # SQLALCHEMY_DATABASE_URL = "sqlite:///./todos.db"
 # postgresql
-SQLALCHEMY_DATABASE_URL = "postgresql://postgres:password@localhost/TodoApplicationDatabase"
+# SQLALCHEMY_DATABASE_URL = "postgresql://postgres:password@localhost/TodoApplicationDatabase"
+# mysql
+SQLALCHEMY_DATABASE_URL="mysql+pymysql://root:password@127.0.0.1:3306/todoapp"
 
 # create an engine, these arguments are specific to sqlite
 # engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
-# postgresql
+# postgresql OR mysql
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
 # instance of database session
