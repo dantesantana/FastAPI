ce5cd436a0abb0c30468fd379e2e6043ed8ee85e
Josh Garvey <12745216+dataGuyJosh@users.noreply.github.com>
2023-04-04 21:16:23 +1000

    Field validation with pydantic

--- fastAPI/books_project/books.py
+++ fastAPI/books_project/books.py
@@ -1,28 +1,65 @@
+from typing import Optional
+
 from fastapi import FastAPI
 # Pydantic is used for data validation and settings management
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 from uuid import UUID
 
 app = FastAPI()
 
-class Book(BaseModel):
-    id: UUID
+
+class Book:
+    id: int
     title: str
     author: str
     description: str
     rating: int
 
+    def __init__(self, id, title, author, description, rating):
+        self.id = id
+        self.title = title
+        self.author = author
+        self.description = description
+        self.rating = rating
 
-BOOKS = []
+
+class BookRequest(BaseModel):
+    id: Optional[int]
+    title: str = Field(min_length=3)
+    author: str = Field(min_length=1)
+    description: str = Field(min_length=1, max_length=100)
+    rating: int = Field(gt=0, lt=6)
 
 
-@app.get('/')
+BOOKS = [
+    Book(1, 'Computer Science Pro', 'codingwithroby', 'A very nice book!', 5),
+    Book(2, 'Be Fast with FastAPI', 'codingwithroby', 'A great book!', 5),
+    Book(3, 'Master Endpoints', 'codingwithroby', 'An awesome book!', 5),
+    Book(4, 'HP1', 'Author 1', 'Book Description', 2),
+    Book(5, 'HP2', 'Author 2', 'Book Description', 3),
+    Book(6, 'HP3', 'Author 3', 'Book Description', 1)
+]
+
+
+@app.get('/books')
 async def read_all_books():
     return BOOKS
 
 # using BaseModel means that FastAPI will validate the request body to make sure
 # each field is supplied and parse types where necessary (e.g. int to string)
-@app.post('/')
-async def create_book(book: Book):
-    BOOKS.append(book)
+
+# creating a request object inheriting from BaseModel means the swagger section for this endpoint will have an example body
+
+
+@app.post('/create-book')
+async def create_book(book_request: BookRequest):
+    # convert BookRequest object into Book object
+    new_book = Book(**book_request.dict())
+    BOOKS.append(find_book_id(new_book))
+    return new_book
+
+
+def find_book_id(book: Book):
+    book.id = 1 if len(BOOKS) == 0 else BOOKS[-1].id + 1
     return book
+    
